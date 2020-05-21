b5cdbe781b47d8ed0124e0b16fb87e5e25fd838a
bosto <bosto@bosto.net>
2020-05-21 22:51:28 +0200

    Added CORS policies.

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,14 @@ namespace AddressBookServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
             services.AddControllers();
         }
 
@@ -34,6 +42,8 @@ namespace AddressBookServer
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors("CorsPolicy");
 
             app.UseRouting();
 
