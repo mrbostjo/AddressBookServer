021a1b658c84554c5595eb91d1420f0bf0aec3df
bosto <bosto@bosto.net>
2020-05-22 08:57:56 +0200

    Added db context for Contact db.

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AddressBookServer.Data;
 
 namespace AddressBookServer
 {
@@ -33,6 +35,9 @@ namespace AddressBookServer
             });
 
             services.AddControllers();
+
+            services.AddDbContext<AddressBookServerContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AddressBookServerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
