eca7fbe06eb272c8263301600dd1989539ebfb7a
bosto <bosto@bosto.net>
2020-05-22 20:26:21 +0200

    Added a class for pagination of results.

---
+++ Controllers/PaginatedList.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBookServer.Controllers
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, string pageIndex, string pageSize)
+        {
+            int _pageIndex = 1;//default page
+            int _pageSize = 10;//default page size
+            if (!String.IsNullOrEmpty(pageIndex))
+            {
+                int.TryParse(pageIndex, out _pageIndex);
+            }
+
+            if (!String.IsNullOrEmpty(pageSize))
+            {
+                int.TryParse(pageSize, out _pageSize);
+
+            }
+            return await CreateAsync(source, _pageIndex, _pageSize);
+        }
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
